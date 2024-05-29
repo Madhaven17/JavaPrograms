d4802ca3bdd1810c0cab200e345a95004952e2d8
madhaven <madhavenmaddy17@gmail.com>
2024-05-29 17:42:10 +0530

    completed the Pronoun Finder and trying to find the noun Words.

--- src/NounFinderGame.java
+++ src/NounFinderGame.java
@@ -1,36 +1,38 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
-public class SentenceFinderGame {
+public class NounFinderGame {
     public static void main(String[] args)  throws Exception {
-        List<String> inputFile = Files.readAllLines(Path.of("/home/msuser1/RECALL ALL LANGUAGES/SentanceFinderGame/src/inputFile.txt"));
+        List<String> inputFile = Files.readAllLines(Path.of("/home/msuser1/GitHub/SentanceFinderGame/src/inputFile.txt"));
 //        List<String> outputFile = Files.readAllLines(Path.of(""));
-
-       if (!inputFile.isEmpty()){
-            for (String input : inputFile){
-                if (input.isEmpty()){
+       if (!inputFile.isEmpty()) {
+           for (String input : inputFile) {
+               if (input.isEmpty()) {
 //                    System.out.println("Line is empty");
-                } else if (input.matches("[0-9a-zA-Z\\s]+")) {
+               } else if (input.matches("[0-9a-zA-Z\\s]+")) {
 //                    System.out.println("Invalid Input");
-                }
-                if (input.matches("[a-zA-Z\\s]+.")) {
+               }
+               if (input.matches("[a-zA-Z\\s]+.")) {
                    String[] splitTheEachSentence = input.split("\\.");
                    for (String sentence : splitTheEachSentence) {
                        String[] splitTheWords = sentence.split(" ");
                        for (String words : splitTheWords) {
+                           List<String> remainingWords = new ArrayList<>();
                            String lowerCaseWords = words.toLowerCase();
                            if (lowerCaseWords.startsWith("her") || lowerCaseWords.startsWith("she")) {
-                               System.out.println("it is pronoun" +" "+ " "+ "--->"+" "+ " "+" "+lowerCaseWords);
-                           }else {
-                               System.out.println(lowerCaseWords);
+//                               System.out.println("it is pronoun" +" "+ " "+ "--->"+" "+ " "+" "+lowerCaseWords);
+                           } else {
+                               remainingWords.add(lowerCaseWords);
+
                            }
                        }
                    }
                }
            }
        }else {
-           System.out.println("The File is Empty");
+           System.out.println("the file is empty");
        }
     }
 }
